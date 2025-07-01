f2f20dbab9a51999f4ac7a03988335785d8253af
Tuhin Karmakar <tuhinkarmakar696@gmail.com>
2025-07-01 22:57:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ Important for Vercel if routing fails
   build: {
-    outDir: 'dist' // Make sure it's 'dist'
+    outDir: 'dist'
   }
-})
+});
